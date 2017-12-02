5179c944a17813ede20de02f7e2c5dc473c0c1ea
Penjani Chavula <chavulapenjani@gmail.com>
2017-12-02 01:45:06 -0800

    Added another detail to the Relationship.java spec

--- Java/src/main/java/relationship.java
+++ Java/src/main/java/relationship.java
@@ -2,7 +2,9 @@ package main.java;
 
 /**
  * Relationship class that ties one member to another. There is a relationship from member A to member B. Once this is
- * determined, it will be stored in the Storage.java in a collection separate from the members.
+ * determined, it will be stored in the Storage.java in a collection separate from the members. A relationship will
+ * also have a field that says whether it is a vertical or horizontal relationship, to help make it easier in sorting
+ * through relationships.
  */
 public class Relationship {
 }
