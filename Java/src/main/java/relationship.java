bfaa5def11391a348d61f2dc18f706c7632f8b84
Penjani Chavula <chavulapenjani@gmail.com>
2017-12-02 01:39:22 -0800

    Added some class specs for relationship.java and storage.java

--- Java/src/main/java/relationship.java
+++ Java/src/main/java/relationship.java
@@ -1,4 +1,8 @@
 package main.java;
 
-public class relationship {
+/**
+ * Relationship class that ties one member to another. There is a relationship from member A to member B. Once this is
+ * determined, it will be stored in the Storage.java in a collection separate from the members.
+ */
+public class Relationship {
 }
--- Java/src/main/java/storage.java
+++ Java/src/main/java/storage.java
@@ -1,4 +1,10 @@
 package main.java;
 
-public class storage {
+/**
+ * Storage facility. Acts like a database, initialized using a file name, reads a file and creates the members,
+ * stores the members in a set(?),  handles the adding member option(?).
+ * In addition, the storage will keep track of the relationships (also probably in a set) and it will determine these
+ * on demand, i.e. We will determine and store a relationship on the go, rather than all of them at once. 
+ */
+public class Storage {
 }
