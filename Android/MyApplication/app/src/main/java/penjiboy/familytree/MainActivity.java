dbf149146f77e72c881f6e050f4511312f39daf3
Penjiboy <chavulapenjani@gmail.com>
2018-07-17 20:58:22 -0700

    Started work new tree activity

--- Android/MyApplication/app/src/main/java/penjiboy/familytree/MainActivity.java
+++ Android/MyApplication/app/src/main/java/penjiboy/familytree/MainActivity.java
@@ -1,7 +1,10 @@
 package penjiboy.familytree;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,17 +22,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Assign each button from the UI onto variable
-        quitButton = (Button) findViewById(R.id.quitButton);
-        loadExistingTreesButton = (Button) findViewById(R.id.loadExistingTreesButton);
-        newTreeButton = (Button) findViewById(R.id.newTreeButton);
-        currentTreeButton = (Button) findViewById(R.id.currentTreeButton);
+        configureButtons();
 
         /*
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
         */
+    }
+
+    private void configureButtons() {
+        //Assign each button from the UI onto variable
+        quitButton = (Button) findViewById(R.id.quitButton);
+        loadExistingTreesButton = (Button) findViewById(R.id.loadExistingTreesButton);
+        newTreeButton = (Button) findViewById(R.id.newTreeButton);
+        currentTreeButton = (Button) findViewById(R.id.currentTreeButton);
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do something in response to button click
+                //quitButton.setTextColor(255);
+            }
+        });
+
+
+        newTreeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CreateNewTreeActivity.class));
+            }
+        });
     }
 
     /**
