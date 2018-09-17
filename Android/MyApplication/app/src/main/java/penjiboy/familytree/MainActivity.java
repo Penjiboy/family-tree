9798a6d00462f21549831b868097d4fd1679bf18
Penjiboy <chavulapenjani@gmail.com>
2018-09-16 20:56:09 -0700

    Fixed up alert dialog behaviour

--- Android/MyApplication/app/src/main/java/penjiboy/familytree/MainActivity.java
+++ Android/MyApplication/app/src/main/java/penjiboy/familytree/MainActivity.java
@@ -8,7 +8,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,44 +18,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private View.OnClickListener newTreeButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // TODO: Implement this
-            setupAlertDialog();
-            dialogBuilder.show();
-            if(!addDatabaseEntry(newTreeName)) {
-                Context context = getApplicationContext();
-                CharSequence text = "Failed to create a new tree, please try again";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            } else {
-                Toast.makeText(
-                        getApplicationContext(),
-                        "Successfully created a new tree",
-                        Toast.LENGTH_SHORT)
-                        .show();
-
-                // Now switch to the activity responsible for adding members to a tree
-            }
-
-        }
-
-        private boolean addDatabaseEntry(String name) {
-            // TODO: Implement this method
-            return false; //change this
-        }
-    };
     private Button loadExistingTreesButton, newTreeButton, currentTreeButton;
     private AlertDialog.Builder dialogBuilder;
-
-    // Used to load the 'native-lib' library on application startup.
-
-    static {
-        System.loadLibrary("native-lib");
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
         */
     }
 
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
     private void configureButtons() {
         //Assign each button from the UI onto variable
         loadExistingTreesButton = (Button) findViewById(R.id.loadExistingTreesButton);
@@ -80,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //startActivity(new Intent(MainActivity.this, CreateNewTreeActivity.class));
-
             }
         });*/
 
@@ -96,10 +66,38 @@ public class MainActivity extends AppCompatActivity {
         //END
     }
 
+    // Create an anonymous implementation of OnClickListener for the newTreeButton
+    private View.OnClickListener newTreeButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // TODO: Implement this
+            setupAlertDialog();
+            dialogBuilder.show();
+
+
+            /*if(!addDatabaseEntry(newTreeName)) {
+                Context context = getApplicationContext();
+                CharSequence text = "Failed to create a new tree, please try again";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            } else {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Successfully created a new tree",
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+                // Now switch to the activity responsible for adding members to a tree
+            }*/
+        }
+    };
+
+
     //setup alertDialog builder for newTreeButton
     private String newTreeName = "";
-
-
+    
     private void setupAlertDialog() {
         //Taken from https://stackoverflow.com/questions/10903754/input-text-dialog-android
         //START
@@ -116,6 +114,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 newTreeName = input.getText().toString();
+
+                if(!addDatabaseEntry(newTreeName)) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Failed to create a new tree, please try again";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                } else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Successfully created a new tree",
+                            Toast.LENGTH_SHORT)
+                            .show();
+
+                    // Now switch to the activity responsible for adding members to a tree
+                }
             }
         });
         dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -126,7 +141,11 @@ public class MainActivity extends AppCompatActivity {
         });
         //END
     }
-    // Create an anonymous implementation of OnClickListener for the newTreeButton
+
+    private boolean addDatabaseEntry(String name) {
+        // TODO: Implement this method
+        return false; //change this
+    }
 
 
     /**
